715c79ff2ec5178ac5915f183f3584dbe09725c3
Carlos Lima <carlos0ficial500@gmail.com>
2024-07-02 22:01:17 -0300

    style: Implementing more styling on my form.

--- src/Layout/Form/style.ts
+++ src/Layout/Form/style.ts
@@ -39,3 +39,48 @@ export const FormLabels = styled.label`
   font-size: 1rem;
   color: #333;
 `;
+
+export const Asterisk = styled.span`
+  color: red;
+`
+
+export const ContainerButtons = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+    gap: 20px;
+    margin: 0 auto;
+    margin-bottom: 20px;
+`
+
+export const SaveButton = styled.button`
+    width: 120px;
+    height: 45px;
+    padding: 12px;
+    align-items: center;
+    border-radius: 50px;
+    border-style: none;
+    background-color: #000000;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+       background-color: #24100B;
+    }
+`
+
+export const CancelButton = styled.button`
+    width: 120px;
+    height: 45px;
+    padding: 12px;
+    align-items: center;
+    border-radius: 50px;
+    border-style: none;
+    background-color: #000000;
+    color: #fff;
+    cursor: pointer;
+
+    &:hover {
+       background-color: #24100B;
+    }
+`
