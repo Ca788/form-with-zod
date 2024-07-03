570dbdf5ad317a5b22d34340c1bd6396105e1d9a
Carlos Lima <carlos0ficial500@gmail.com>
2024-07-02 21:57:33 -0300

    feat: Implementing validation to input fields witch zod.

--- src/Layout/Form/index.tsx
+++ src/Layout/Form/index.tsx
@@ -1,18 +1,86 @@
-import { Container, FormContainer, FormInputs, FormLabels, Input, InputGroup } from "./style";
+import {
+  Asterisk,
+  Container,
+  FormContainer,
+  FormInputs,
+  FormLabels,
+  Input,
+  InputGroup,
+  ContainerButtons,
+  SaveButton,
+  CancelButton
+} from "./style";
 import { useForm } from 'react-hook-form';
 import { cpfMask } from "../../utils/masks";
 import { useState } from "react";
-import { Button } from "../../components/button";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+
+/**
+ * TODO: Add form validation
+ * [X] Validação / Transformação.
+ * [] Fields Arrays.
+ * [] Upload.
+ * [] Compistation pattern.
+ */
+
+const CreateUserFormSchema = z.object({
+  name: z.string().min(1, 'Nome é obrigatório').transform(name => {
+    return name.trim().split(' ').map(word => {
+      return word[0].toLocaleUpperCase().concat(word.substring(1))
+    }).join(' ')
+  }),
+  email: z.string().min(1, 'Email é obrigatório').email('Formato de e-mail inválido'),
+  cpf: z.string().min(1, 'CPF é obrigatório'),
+  birthDate: z.string().min(1, 'Data de nascimento é obrigatório'),
+  state: z.string(),
+  city: z.string(),
+  neighborhood: z.string(),
+  street: z.string(),
+  number: z.string(),
+  complement: z.string(),
+});
+
+type CreateUserFormData = z.infer<typeof CreateUserFormSchema>
 
 export const Form = () => {
   const [cpf, setCpf] = useState("");
+  const [output, setOutput] = useState("");
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors }
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(CreateUserFormSchema),
+  });
 
   const formatCpf = (e: any) => {
     const value = e.target.value;
     setCpf(cpfMask(value));
   };
 
-  const { register } = useForm();
+  const createUser = (data: any) => {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
+
+  const clearInput = () => {
+    reset({
+      name: '',
+      email: '',
+      cpf: '',
+      birthDate: '',
+      state: '',
+      city: '',
+      neighborhood: '',
+      street: '',
+      number: '',
+      complement: '',
+    });
+    setCpf("");
+  }
 
   return (
     <>
@@ -21,99 +89,97 @@ export const Form = () => {
           <h1>Formulário de cadastro</h1>
           <Container>
             <InputGroup>
-              <FormLabels>Nome</FormLabels>
+              <FormLabels htmlFor="name">Nome <Asterisk>*</Asterisk></FormLabels>
               <Input
                 type="text"
-                placeholder="Nome"
                 {...register("name")}
               />
+              {errors.name && <span>{errors.name.message}</span>}
             </InputGroup>
             <InputGroup>
-              <FormLabels>Email</FormLabels>
+              <FormLabels htmlFor="email">Email <Asterisk>*</Asterisk></FormLabels>
               <Input
                 type="email"
-                placeholder="Email"
                 {...register("email")}
               />
+              {errors.email && <span>{errors.email.message}</span>}
             </InputGroup>
           </Container>
           <Container>
             <InputGroup>
-              <FormLabels>CPF</FormLabels>
+              <FormLabels htmlFor="cpf">CPF <Asterisk>*</Asterisk></FormLabels>
               <Input
                 type="text"
-                placeholder="CPF"
                 value={cpf}
                 {...register("cpf")}
                 onChange={formatCpf}
               />
+              {errors.cpf && <span>{errors.cpf.message}</span>}
             </InputGroup>
             <InputGroup>
-              <FormLabels>Data de nascimento</FormLabels>
+              <FormLabels htmlFor="birthDate">Data de nascimento <Asterisk>*</Asterisk></FormLabels>
               <Input
                 type="date"
-                placeholder="Data de nascimento"
                 {...register("birthDate")}
               />
+              {errors.birthDate && <span>{errors.birthDate.message}</span>}
             </InputGroup>
           </Container>
           <Container>
             <InputGroup>
-              <FormLabels htmlFor="state">UF</FormLabels>
+              <FormLabels htmlFor="zipcode">UF</FormLabels>
               <Input
                 type="text"
-                placeholder="UF"
                 {...register("state")}
               />
             </InputGroup>
             <InputGroup>
-              <FormLabels htmlFor="state">Cidade</FormLabels>
+              <FormLabels htmlFor="city">Cidade</FormLabels>
               <Input
                 type="text"
-                placeholder="Cidade"
                 {...register("city")}
               />
             </InputGroup>
           </Container>
           <Container>
             <InputGroup>
-              <FormLabels htmlFor="state">Bairro</FormLabels>
+              <FormLabels htmlFor="neighborhood">Bairro</FormLabels>
               <Input
                 type="text"
-                placeholder="Bairro"
                 {...register("neighborhood")}
               />
             </InputGroup>
             <InputGroup>
-              <FormLabels htmlFor="state">Rua</FormLabels>
+              <FormLabels htmlFor="street">Rua</FormLabels>
               <Input
                 type="text"
-                placeholder="Rua"
                 {...register("street")}
               />
             </InputGroup>
           </Container>
           <Container>
             <InputGroup>
-              <FormLabels htmlFor="state">Número</FormLabels>
+              <FormLabels htmlFor="number">Número</FormLabels>
               <Input
                 type="text"
-                placeholder="Número"
                 {...register("number")}
               />
             </InputGroup>
             <InputGroup>
-              <FormLabels htmlFor="state">Complemento</FormLabels>
+              <FormLabels htmlFor="complement">Complemento</FormLabels>
               <Input
                 type="text"
-                placeholder="Complemento"
                 {...register("complement")}
               />
             </InputGroup>
           </Container>
         </FormInputs>
       </FormContainer>
-      <Button />
+      <ContainerButtons>
+        <SaveButton onClick={handleSubmit(createUser)}>Salvar</SaveButton>
+        <CancelButton onClick={clearInput}>Cancelar</CancelButton>
+      </ContainerButtons>
+      <pre style={{ display: "flex", justifyContent: "center" }}>{output}</pre>
     </>
   );
 };
