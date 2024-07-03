6b0cb02a5fbf488fec359b14dd6e298f1d4c8080
Carlos Lima <carlos0ficial500@gmail.com>
2024-07-02 22:00:03 -0300

    fix: Removing submit in button component.

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,15 +1,21 @@
 import { useForm } from "react-hook-form";
 import { CancelButton, ContainerButtons, SaveButton } from "./style"
+import { useState } from "react";
 
 export const Button = () => {
+  const [output, setOutput] = useState("");
   const { handleSubmit } = useForm();
 
   const createUser = (data: any) => {
-    console.log(data);
+    setOutput(JSON.stringify(data, null, 2));
   }
 
-  return <ContainerButtons>
-    <SaveButton onClick={handleSubmit(createUser)}>Salvar</SaveButton>
-    <CancelButton>Cancelar</CancelButton>
-  </ContainerButtons>
+  return (
+    <>
+      <ContainerButtons>
+        <SaveButton onClick={handleSubmit(createUser)}>Salvar</SaveButton>
+        <CancelButton>Cancelar</CancelButton>
+      </ContainerButtons>
+    </>
+  )
 }
