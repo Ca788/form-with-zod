e51beb525d71e534f2fca98136256c7c1413d8a3
Carlos Lima <carlos0ficial500@gmail.com>
2024-07-03 23:44:35 -0300

    feat: Implementing mask to birthDate input.

--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -6,3 +6,14 @@ export const cpfMask = (value: string) => {
     .replace(/(\d{3})(\d{1,2})/, "$1-$2")
     .replace(/(-\d{2})\d+?$/, "$1")
 };
+
+export const formatBirthDate = (value: string) => {
+  const cleanValue = value.replace(/\D/g, '');
+  if (cleanValue.length <= 2) {
+    return cleanValue.replace(/^(\d{2})/, '$1');
+  } else if (cleanValue.length <= 4) {
+    return cleanValue.replace(/^(\d{2})(\d{2})/, '$1/$2');
+  } else {
+    return cleanValue.replace(/^(\d{2})(\d{2})(\d{4}).*/, '$1/$2/$3');
+  }
+};
